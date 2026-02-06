4c0dd39396f0872663a501613e5104b3376b01cf
carr39 <carr39@hotmail.com>
2026-02-06 14:39:54 -0600

    Create page.tsx

---
+++ app/cliente/pago/page.tsx
@@ -0,0 +1,18 @@
+export default function PagoCliente() {
+  return (
+    <main>
+      <h1>Pago del servicio</h1>
+      <p>Transferencia o SPEI</p>
+
+      <ul>
+        <li>Banco: XXXXX</li>
+        <li>CLABE: XXXXXXXX</li>
+        <li>Concepto: Servicio Omnifix</li>
+      </ul>
+
+      <p>
+        Al realizar el pago, el servicio se considera concluido.
+      </p>
+    </main>
+  );
+}
